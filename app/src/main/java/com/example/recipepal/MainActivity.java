3816abc8bee6298ee2e29325edf63737afd52dce
hbrender <hbrender615@gmail.com>
2019-12-01 21:33:02 -0800

    removing cookbook page

--- app/src/main/java/com/example/recipepal/MainActivity.java
+++ app/src/main/java/com/example/recipepal/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_recipes, R.id.navigation_cookbooks, R.id.navigation_grocery_list)
+                R.id.navigation_recipes, R.id.navigation_grocery_list)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
